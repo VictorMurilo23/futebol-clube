e012dc0f10edfa6f14d736180744de6371e7dad8
Victor-Murilo <victor2706.vm@gmail.com>
2022-11-22 19:22:22 -0300

    Cumpri os requisitos 5 e 7

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,4 +1,5 @@
 import { compare } from 'bcryptjs';
+import * as Joi from 'joi';
 import createToken from '../utils/token';
 import UserInfo from '../types/UserInfo';
 import UserModel from '../database/models/UserModel';
@@ -8,23 +9,34 @@ import IUserService from '../interfaces/IUserService';
 export default class UserService implements IUserService {
   constructor(private userModel = UserModel) {}
 
-  private static async validateLogin(dbInfo: UserInfo | null, reqBodyInfo: UserLoginBody) {
-    if (dbInfo === null) {
-      throw new Error('Incorrect email or password');
+  private static validateReqBody(reqBody: UserLoginBody): UserLoginBody {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+
+    const { error, value } = schema.validate(reqBody);
+    if (error) {
+      throw new Error('All fields must be filled');
     }
-    const match = await compare(reqBodyInfo.password, dbInfo.password);
-    if (!match) {
+
+    return value;
+  }
+
+  private static async validateLogin(dbInfo: UserInfo | null, reqBodyInfo: UserLoginBody) {
+    if (dbInfo === null || await compare(reqBodyInfo.password, dbInfo.password) === false) {
       throw new Error('Incorrect email or password');
     }
     return { ...dbInfo, password: '_' };
   }
 
   public async login(reqBody: UserLoginBody): Promise<string> {
+    const validUserInfo = UserService.validateReqBody(reqBody);
     const dbInfo: UserInfo | null = await this.userModel
-      .findOne({ where: { email: reqBody.email }, attributes: { exclude: ['id'] },
+      .findOne({ where: { email: validUserInfo.email }, attributes: { exclude: ['id'] },
       });
 
-    const userInfo: UserInfo = await UserService.validateLogin(dbInfo, reqBody);
+    const userInfo: UserInfo = await UserService.validateLogin(dbInfo, validUserInfo);
     const token = createToken(userInfo);
     return token;
   }
